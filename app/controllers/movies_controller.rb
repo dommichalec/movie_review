9644195c494c9bce6b06a364c08a47b4a006b3a5
Dominic Michalec <dominicjjmichalec@gmail.com>
2016-03-31 13:51:47 -0400

    add show and edit

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,4 +2,19 @@ class MoviesController < ApplicationController
   def index
     @movies = Movie.all
   end
+
+  def show
+    @movie = Movie.find(params[:id])
+  end
+
+  def edit
+    @movie = Movie.find(params[:id])
+  end
+
+  def update
+    @movie = Movie.find(params[:id])
+    movie_params = params.require(:movie).permit(:title, :description)
+    @movie.update(movie_params)
+    redirect_to movie_path(@movie.id)
+  end
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -3,4 +3,8 @@ class Movie < ActiveRecord::Base
   def popular?
     rating.to_f > 5.0
   end
+
+  def format_default_date
+    release_date.to_formatted_s(:long)
+  end
 end
